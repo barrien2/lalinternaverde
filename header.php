25f78147133df6a5a6593965b1957c011acd105e
barrien2 <xavib99@gmail.com>
2019-11-12 18:29:21 +0100

    Nou header

--- header.php
+++ header.php
@@ -8,13 +8,46 @@
       <!-- Navigation. We hide it in small screens. -->
       <nav class="mdl-navigation mdl-layout--large-screen-only">
         <a class="mdl-navigation__link" href="index.php">Inici</a>
-        <a class="mdl-navigation__link" href="novainsignia.php">Inserir nova insignia</a>
-        <a class="mdl-navigation__link" href="atorgarinsignia.php">Otorgar insignia</a>
+        
         <a class="mdl-navigation__link" href="treballadors.php">Llista treballadors</a>
-        <a class="mdl-navigation__link" href="insignies.php">Llista insignies</a>
-        <a class="mdl-navigation__link" href="consultaratorgades.php">Llista atorgades</a>
+        
+        
         <a class="mdl-navigation__link" href="inserirtreballadors.php">Inserir treballadors</a>
       </nav>
     </div>
-  </header>';
-?>
+  </header>
+  
+
+
+
+  <nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left">
+
+        <ul class="uk-navbar-nav">
+            <li><a href="index.php">Inici</a></li>
+            <li>
+                <a href="insignies.php">Insignies</a>
+                <div class="uk-navbar-dropdown">
+                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                        <li><a href="novainsignia.php">Nova insignia</a></li>
+                        <li><a href="atorgarinsignia.php">Atornar insignia</a></li>
+                        <li><a href="consultaratorgades.php">Llista atorgades</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+            <a href="treballadors.php">Treballadors</a>
+            <div class="uk-navbar-dropdown">
+                <ul class="uk-nav uk-navbar-dropdown-nav">
+                    <li><a href="inserirtreballadors.php">Inserir treballadors</a></li>
+                </ul>
+            </div>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+  
+  
+  
+  ';
---
+++ llibreriesCssJs.php
@@ -0,0 +1,9 @@
+<?php 
+echo '<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js"></script>';
+
+?>
